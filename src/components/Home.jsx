5b64e1a65ca44ed1fcae8b56d6be75b59905e135
Kamran <kamran.khan.03112002@gmail.com>
2024-08-23 18:22:27 +0530

    Changed few icons and pictures

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImage from "../assets/heroImage.jpeg";
+import HeroImage from "../assets/heroImage.jpg";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 import { TypeAnimation } from "react-type-animation";
